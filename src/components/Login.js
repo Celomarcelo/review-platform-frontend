122501f881722585ae9f34b813497b2f51973347
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-11-09 11:53:18 +0000

    added url absolut

--- src/components/Login.js
+++ src/components/Login.js
@@ -48,7 +48,7 @@ const Login = () => {
         event.preventDefault();  // Prevents default form submission
 
         // Send POST request to the login endpoint
-        axios.post('${process.env.REACT_APP_API_URL}/api/login/', {
+        axios.post('https://new-books-backend-3c4d25dea9f8.herokuapp.com/api/login/', {
             username,  // Send username from the form input
             password,  // Send password from the form input
         })
